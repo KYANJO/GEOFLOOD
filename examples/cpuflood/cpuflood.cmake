4829fb988263b0f3c56baa72f995b7ee3f8e3e52
KYANJO <brian.kyanjo@aims.ac.rw>
2024-09-11 22:51:05 -0400

    wip: implemented thhe CPU version of the missoula flood

--- examples/cpuflood/cpuflood.cmake
+++ examples/cpuflood/cpuflood.cmake
@@ -6,5 +6,5 @@
 
 add_subdirectory(cpuflood/teton)
 add_subdirectory(cpuflood/malpasset)
-add_subdirectory(cpuflood/triton)
+add_subdirectory(cpuflood/missoula)
 add_subdirectory(cpuflood/benchmark_tests)
